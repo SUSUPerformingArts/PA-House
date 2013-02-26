35b2807ccae8f97ad092e43313ec26d7dfd38de7
Joe Hart <joehart42@googlemail.com>
2013-02-26 20:38:01 +0000

    Added Image Upload
    Now image uploads and changedhow date added is changed

--- additem.php
+++ additem.php
@@ -1,16 +1,79 @@
 <?php
 
 include 'header.php';
+?>
 
-if ($_SERVER['REQUEST_METHOD'] == "POST")
-{
+<?php
+if(!empty($_POST)){
+########   Basic Settings    ########
+// Your Root Directory 
+    $filename = "";
+$base = "./";
+//The Directory Where Files Will Be Uploaded
+$dir = "/uploads/";
+// Your Site URL
+$url = "http://perform.susu.org/house";
+####################################
+$allowded = array("image/bmp","image/pjpeg" , "image/jpeg" , "image/gif" , "image/png" , "image/jpg");
+$name = $_POST[name];
+//$type = $_POST[type];
+$rand = rand( 100 , 20000);
+echo $_FILES[file][type];
+echo'<br>';
+echo $_FILES[file][tmp_name];
+echo'<br>';
+        if(empty($_POST[name])){
+        echo "You Forgot To Fill the required Feild Please Fix The Error";
+                                        }
+        elseif(!in_array($_FILES[file][type] , $allowded)){
+        echo "Un Expected File Format Only Images Supported";
+                                            }
+                 elseif($_FILES[file][size] >= 500000){
+                 echo "Too Large File!!! Maximum File Size Allowded IS 500 kb!!";
+                    }
+                                            
+                else{
+             $filename = $name."_"."$type"."_".$rand."_".$_FILES[file][name];
+                $file = $base.$dir.$filename;
+                echo $file;
+             $upload = move_uploaded_file($_FILES[file][tmp_name] , $file);
+                chmod($file , 0755);
+                echo $_FILES[file][size];
+                if($upload){
+             echo "File Uploaded Sucessfully !!! <br><a href=\"".$url.$dir.$filename. "\">Click Here To view your File</a> &nbsp; <br>";
+             echo "Your Code To Post That Image In Forum Is <b> : [IMG]".$url.$dir.$filename."[/IMG]</b><br>";
+             echo "Just Copy the Above Code And paste It Where You Wana See this Image In the forum";
+                
+                
+                
+                        }
+                }
+    //}
+//if ($_SERVER['REQUEST_METHOD'] == "POST")
+//{
 
-$name=$_POST['name'];
+
+//$name=$_POST['name'];
 $description=$_POST['description'];
 $added_by=$_POST['added_by'];
+//$dateAdded = CURDATE();
 
-$query = "INSERT INTO `house_objects` (`id`, `name`, `description`, `status`, `added_by`, `container_id`) VALUES ('','$name','$description','In House','$added_by',0)";
+$query = "INSERT INTO  `perform`.`house_objects` (
+`id` ,
+`name` ,
+`description` ,
+`status` ,
+`added_by` ,
+`container_id` ,
+`imageURL`
+)
+VALUES (
+'', '$name', '$description', 'In House', '$added_by', '0' ,'$filename'
+);
+";
+
 include './database_connect.php';
+
 
 if($result){
 print("
@@ -20,6 +83,7 @@ print("
 
 
     ");
+echo "ID: ".$id;
 } else {
 print("
     <div class='alert alert-error'>
@@ -30,18 +94,21 @@ print("
     ");
 }
 
-}?>
+}
+?>
 
 
     <div class="row-fluid">
         <div class="span4">
-        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data" name="form1" id="form1">
             <fieldset>
             <legend>Add Item To Database</legend>
             <label>Name of Item</label>
             <input type="text" name="name" class="span12" placeholder="Type name of item...">
             <label>Description</label>
             <textarea name="description" class="span12" rows="5"></textarea>
+            <label>Select Your File : </label>
+            <input type="file" name="file" />
             <label>Your Name</label>
             <input type="text" name="added_by"  class="span12" placeholder="Type your name here...">
             <p><input type="submit" class="btn span12"></p>
--- database_connect.php
+++ database_connect.php
@@ -4,6 +4,7 @@ include 'database_vars.php';
 $mysql_connection = mysql_connect($server,$username,$password);
 @mysql_select_db($database) or die( "Unable to select database");
 $result=mysql_query($query);
+$id = mysql_insert_id();
 
 $last_insert_id=mysql_insert_id($mysql_connection);
 
